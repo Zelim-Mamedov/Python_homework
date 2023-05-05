9ecd99491b735380f567e565e0255b2d789c4c01
Zelim <zelya140489@gmail.com>
2023-05-05 16:03:51 +0600

    Не решили ничего

---
+++ Home_work_9_Seminar/Task_9_42.py
@@ -0,0 +1,3 @@
+# 9.2[42]: Работа в Google Colab. Файл california_housing_train.csv, который находится в папке sample_data
+# Узнать какая максимальная households в зоне минимального значения population
+
